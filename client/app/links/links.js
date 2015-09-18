051b8af669d2600eb52a0fb8cf3bde9f7a074cb0
naomiajacobs <naomiajacobs@gmail.com>
2015-09-18 15:52:54 -0700

    Fix indentation

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,12 +2,11 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   $scope.data = {};
-
   $scope.getLinks = function () {
-  	Links.getLinks().then(function (response) {
-  		console.log(response.data);
-  		$scope.data.links = response.data;
-  	});
+    Links.getLinks().then(function (response) {
+      console.log(response.data);
+      $scope.data.links = response.data;
+    });
   };
 
   $scope.getLinks();
