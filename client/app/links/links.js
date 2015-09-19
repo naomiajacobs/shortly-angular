2190bca4523a43c69795810be44185aa2eb36d0f
naomiajacobs <naomiajacobs@gmail.com>
2015-09-18 17:31:15 -0700

    Fix redirecting

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,6 +2,7 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   $scope.data = {};
+  
   $scope.getLinks = function () {
     Links.getLinks().then(function (response) {
       $scope.data.links = response.data;
