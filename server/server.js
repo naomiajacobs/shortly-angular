f00462e3dd2b6cfbc63f0dc5d0611cb21f359de0
naomiajacobs <naomiajacobs@gmail.com>
2015-09-19 10:47:55 -0700

    Set up mongo server on Azure and connected to it

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 
 var app = express();
 
-mongoose.connect('mongodb://localhost/shortly'); // connect to mongo database named shortly
+mongoose.connect(require('./mongourl.js')); // connect to mongo database named shortly
 
 // configure our server with all the middleware and and routing
 require('./config/middleware.js')(app, express);
