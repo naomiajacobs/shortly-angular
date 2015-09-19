c81697e263b1ea5aa0c56e099f4b97811dbb90af
naomiajacobs <naomiajacobs@gmail.com>
2015-09-18 17:21:36 -0700

    Implements link views

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -5,11 +5,17 @@ angular.module('shortly.shorten', [])
 
   $scope.pending = false;
 
+
   $scope.addLink = function () {
     $scope.pending = true;
     Links.postLink($scope.link).then(function (response) {
-      $scope.pending = false;
       $scope.link = {};
+    })
+    .catch(function (err) {
+      console.log('Error adding link: ', err);
+    })
+    .then(function () {
+      $scope.pending = false;
     });
   };
 
